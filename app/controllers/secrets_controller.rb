791bd422d65ca475d387bbf834a534f1b2c8b8d0
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-02-03 17:03:02 -0700

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,15 +2,13 @@ class SecretsController < ApplicationController
     before_action :require_login
 
     def show
-        if session[:name] 
 
-        else
-            redirect_to '/sessions/new'
-        end
-        
     end
 
     def require_login
-        return head(:forbidden) unless session.include? :name
+        if session.include? :name
+        else
+            redirect_to '/sessions/new'
+        end
     end
 end

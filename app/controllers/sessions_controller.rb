56d420f8477e3fb89494b8904b4d51e2b93c96d4
Erick Castille <ecastille924@gmail.com>
2021-02-09 20:24:56 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def create 
+        if params[:name] && params[:name].present?
+            session[:name] = params[:name]
+            redirect_to root_path
+        else
+            redirect_to '/login'
+        end
+    end
+
+    def destroy 
+        session.delete :name 
+        redirect_to '/login'
+    end
+end
